ecc296c0f9034dc6b026ab23d4080a1b98a3c0f0
yugal18 <yugalchambhare2001@gmail.com>
2025-01-17 12:58:34 +0530

    add utils

--- src/logger.py
+++ src/logger.py
@@ -3,17 +3,24 @@ import os
 from datetime import datetime
 
 
+
+# Define the log file name
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path, exist_ok = True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+# Define the logs directory path
+logs_dir = os.path.join(os.getcwd(), "logs")
 
+# Ensure the logs directory exists
+os.makedirs(logs_dir, exist_ok=True)
 
+# Full path to the log file
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Configure the logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format = "[%(asctime)s] %(lineno)d %(levelname)s - %(message)s",
-    level = logging.INFO, # This is the level of logging
-    
+    format="[%(asctime)s] %(lineno)d %(levelname)s - %(message)s",
+    level=logging.INFO,  # Set the logging level
 )
+
 
